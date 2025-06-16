a55311d4bfa5b554bc441ea0a30e5dbc6eb173e5
Keshav <keshavmishra1001@gmail.com>
2025-06-16 22:25:27 +0530

    Updated emailjs config

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,4 +1,3 @@
-
 import {
   Instagram,
   Linkedin,
@@ -6,29 +5,51 @@ import {
   MapPin,
   Phone,
   Send,
-  Twitch,
-  Twitter,
+  TwitchIcon,
+  TwitterIcon,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
 
 export const ContactSection = () => {
   const { toast } = useToast();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const formRef = useRef();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
     setIsSubmitting(true);
 
-    setTimeout(() => {
-      toast({
-        title: "Message sent!",
-        description: "Thank you for your message. I'll get back to you soon.",
-      });
-      setIsSubmitting(false);
-    }, 1500);
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID, // replace with your EmailJS service ID
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // replace with your EmailJS template ID
+        formRef.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY // replace with your EmailJS public key
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast({
+            title: "Message sent!",
+            description:
+              "Thank you for your message. I'll get back to you soon.",
+          });
+          setIsSubmitting(false);
+          formRef.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          toast({
+            title: "Error!",
+            description: "Something went wrong. Please try again later.",
+          });
+          setIsSubmitting(false);
+        }
+      );
+    
   };
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
@@ -48,75 +69,79 @@ export const ContactSection = () => {
               {" "}
               Contact Information
             </h3>
+            {/* Transparent card with border and shadow */}
+            <div className="bg-card/60 backdrop-blur-none border border-border rounded-lg shadow-lg p-8">
+              <div className="space-y-6 justify-center">
+                <div className="flex  items-center space-x-4">
+                  <div className="p-3 rounded-full bg-primary/10 ">
+                    <Mail className="h-6 w-6 text-primary" />{" "}
+                  </div>
+                  <div>
+                    {/* <h4 className="font-medium"> Mail :</h4> */}
+                    <a
+                      href="mailto:keshavmishra1001@gmail.com"
+                      className="text-muted-foreground hover:text-primary transition-colors text-lg"
+                    >
+                      keshavmishra1001@gmail.com
+                    </a>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <div className="p-3 rounded-full bg-primary/10">
+                    <Phone className="h-6 w-6 text-primary" />{" "}
+                  </div>
+                  <div>
+                    {/* <h4 className="font-medium"> Phone :</h4> */}
+                    <a
+                      href="tel:+917981401547"
+                      className="text-muted-foreground hover:text-primary transition-colors text-lg"
+                    >
+                      +917981401547
+                    </a>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <div className="p-3 rounded-full bg-primary/10">
+                    <MapPin className="h-6 w-6 text-primary" />{" "}
+                  </div>
+                  <div>
+                    {/* <h4 className="font-medium"> Location :</h4> */}
+                    <a className="text-muted-foreground hover:text-primary transition-colors text-lg">
+                      Hyderabad, India
+                    </a>
+                  </div>
+                </div>
+              </div>
 
-            <div className="space-y-6 justify-center">
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Mail className="h-6 w-6 text-primary" />{" "}
-                </div>
-                <div>
-                  <h4 className="font-medium"> Mail :</h4>
+              <div className="pt-8">
+                <h4 className="font-medium mb-4"> Connect With Me</h4>
+                <div className="flex space-x-4 justify-center">
                   <a
-                    href="mailto:hello@gmail.com"
-                    className="text-muted-foreground hover:text-primary transition-colors"
+                    href="https://www.linkedin.com/in/keshavmishra10/"
+                    target="_blank"
                   >
-                    keshavmishra1001@gmail.com
+                    <Linkedin />
                   </a>
-                </div>
-              </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Phone className="h-6 w-6 text-primary" />{" "}
-                </div>
-                <div>
-                  <h4 className="font-medium"> Phone :</h4>
+                  <a href="https://x.com/K_Mishra1001" target="_blank">
+                    <TwitterIcon />
+                  </a>
                   <a
-                    href="tel:+11234567890"
-                    className="text-muted-foreground hover:text-primary transition-colors"
+                    href="https://www.instagram.com/__kshv_mishra__/"
+                    target="_blank"
                   >
-                    +(91)-7981401547
+                    <Instagram />
+                  </a>
+                  <a href="#" target="_blank">
+                    <TwitchIcon />
                   </a>
                 </div>
-              </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <MapPin className="h-6 w-6 text-primary" />{" "}
-                </div>
-                <div>
-                  <h4 className="font-medium"> Location :</h4>
-                  <a className="text-muted-foreground hover:text-primary transition-colors">
-                    Hyderabad, India
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-8">
-              <h4 className="font-medium mb-4"> Connect With Me</h4>
-              <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
-                  <Linkedin />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitter />
-                </a>
-                <a href="#" target="_blank">
-                  <Instagram />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitch />
-                </a>
               </div>
             </div>
           </div>
 
-          <div
-            className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
-          >
+          <div className="bg-card p-8 rounded-lg shadow-xs">
             <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
-
-            <form className="space-y-6">
+            <form className="space-y-6" ref={formRef} onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="name"
@@ -131,7 +156,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Mark Zuckerberg..."
+                  placeholder="John Doe"
                 />
               </div>
 
@@ -149,7 +174,26 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="markzukerberg@meta.com"
+                  placeholder="johndoe@gmail.com"
+                />
+              </div>
+
+              {/* Subject field added here */}
+              <div>
+                <label
+                  htmlFor="subject"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="Subject"
                 />
               </div>
 
